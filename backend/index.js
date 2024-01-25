66b308c284f1c310743314d48f6a27afc4ef0326
bunee <buneeisslo@gmail.com>
2024-01-25 22:51:50 +0530

    Refactor: get port number from env

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,10 @@ const app = express();
 app.use("/api/v1", rootRouter);
 
 const MONGO_URL = process.env.MONGO_URL;
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 mongoose.connect(MONGO_URL)
     .then((res) => {
-        app.listen(PORT, () => console.log("Server up and running!"));
+        app.listen(PORT, () => console.log(`Server up and running on port:${PORT}`));
     })
     .catch((err) => {
         console.log("Oops, something went wrong.", err);
