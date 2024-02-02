ec91759c1d8ca6812c17aae3bf3f98c0ce6188d4
bunee <buneeisslo@gmail.com>
2024-02-02 18:57:54 +0530

    Init vite react app with shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl py-4">Payup</h1>
+      <Button>Get started</Button>
+    </div>
+  );
+};
+
+export default App;
