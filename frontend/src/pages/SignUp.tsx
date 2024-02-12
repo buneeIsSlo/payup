10d80500633223c6e837d2d940c1dbdd0d20c2f6
bunee <buneeisslo@gmail.com>
2024-02-12 19:35:18 +0530

    Feat: sign up form validation

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -11,8 +11,98 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Logo from "@/components/Logo";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+type TFormData = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+};
+
+const ErrorMessage = ({ id, message }: { id: string; message: string }) => {
+  return (
+    <p
+      id={id}
+      className="text-destructive text-xs font-semibold absolute -bottom-4 left-0"
+      aria-live="polite"
+      aria-atomic="true"
+    >
+      <i>{message}</i>
+    </p>
+  );
+};
+
+async function validateFormData(data: TFormData) {
+  try {
+    const response = await fetch("http://localhost:5001/api/v1/user/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const jsonData = await response.json();
+    return jsonData;
+  } catch (err) {
+    return err;
+  }
+}
 
 const SignUp = () => {
+  const [formData, setFormData] = useState<TFormData>({
+    firstName: "",
+    lastName: "",
+    username: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState<Record<keyof TFormData, string>>({
+    firstName: "",
+    lastName: "",
+    username: "",
+    password: "",
+  });
+  const [isValidatingForm, setIsValidatingForm] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.currentTarget;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: "", // Clear error message when input changes
+    }));
+  }
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      setIsValidatingForm(true);
+      const response = await validateFormData(formData);
+      if (response.errors) {
+        const entries = Object.entries(response.errors);
+        for (const [type, message] of entries) {
+          setErrors((prevData) => ({
+            ...prevData,
+            [type]: message,
+          }));
+        }
+      } else {
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      console.error("Error validating form data:", error);
+    } finally {
+      setIsValidatingForm(false);
+    }
+  }
+
   return (
     <main className="grid place-content-center h-[100svh]">
       <div className="absolute inset-0 -z-10 h-full w-full bg-slate-50 bg-[radial-gradient(#e6e6e6_1px,transparent_1px)] [background-size:16px_16px]"></div>
@@ -27,31 +117,82 @@ const SignUp = () => {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-2">
+          <form onSubmit={handleSubmit}>
+            <div className="grid w-full items-center gap-5">
+              <div className="flex flex-col space-y-2 relative">
                 <Label htmlFor="first-name">First Name</Label>
-                <Input type="text" id="first-name" placeholder="Cool" />
+                <Input
+                  type="text"
+                  id="first-name"
+                  name="firstName"
+                  placeholder="Cool"
+                  onChange={handleChange}
+                  aria-describedby="first-name-error"
+                />
+                {errors.firstName && (
+                  <ErrorMessage
+                    id={"first-name-error"}
+                    message={errors.firstName}
+                  />
+                )}
               </div>
-              <div className="flex flex-col space-y-2">
+              <div className="flex flex-col space-y-2 relative">
                 <Label htmlFor="last-name">Last Name</Label>
-                <Input type="text" id="last-name" placeholder="Name" />
+                <Input
+                  type="text"
+                  id="last-name"
+                  name="lastName"
+                  placeholder="Name"
+                  onChange={handleChange}
+                  aria-describedby="last-name-error"
+                />
+                {errors.lastName && (
+                  <ErrorMessage
+                    id={"last-name-error"}
+                    message={errors.lastName}
+                  />
+                )}
               </div>
-              <div className="flex flex-col space-y-2">
+              <div className="flex flex-col space-y-2 relative">
                 <Label htmlFor="email">Email</Label>
                 <Input
                   type="email"
                   id="email"
+                  name="username"
                   placeholder="coolname@mail.com"
+                  onChange={handleChange}
+                  aria-describedby="email-error"
                 />
+                {errors.username && (
+                  <ErrorMessage id={"email-error"} message={errors.username} />
+                )}
               </div>
-              <div className="flex flex-col space-y-2">
+              <div className="flex flex-col space-y-2 relative">
                 <Label htmlFor="password">Password</Label>
-                <Input type="password" id="password" />
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={handleChange}
+                  aria-describedby="password-error"
+                />
+                {errors.password && (
+                  <ErrorMessage
+                    id={"password-error"}
+                    message={errors.password}
+                  />
+                )}
               </div>
-              <Button type="submit" className="w-full">
-                Sign Up
-              </Button>
+              {!isValidatingForm && (
+                <Button type="submit" className="w-full mt-1">
+                  Sign Up
+                </Button>
+              )}
+              {isValidatingForm && (
+                <Button className="w-full mt-1" disabled>
+                  Please wait
+                </Button>
+              )}
             </div>
           </form>
         </CardContent>
