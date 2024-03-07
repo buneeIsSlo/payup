56769263209b4e0c0b7641f0ea61254c3966c112
bunee <buneeisslo@gmail.com>
2024-03-07 22:33:51 +0530

    Style: page not found component

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,5 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>404 Not Found</div>;
+  return (
+    <main className="container min-h-[100dvh] grid place-content-center">
+      <div className="flex flex-col py-6 lg:py-8">
+        <h1 className="text-4xl font-bold mx-auto lg:text-8xl">404</h1>
+        <p className="text-base text-muted-foreground mx-auto lg:text-2xl">
+          Sorry, this page does not exist.
+        </p>
+      </div>
+      <Button variant={"outline"} className="w-fit mx-auto">
+        <Link to={"/"}>Go Home</Link>
+      </Button>
+    </main>
+  );
 };
 
 export default NotFound;
