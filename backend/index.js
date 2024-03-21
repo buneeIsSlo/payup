97c38ac24f4cb4b4b8ef2d8528401b414e46d3ee
bunee <buneeisslo@gmail.com>
2024-03-21 20:35:11 +0530

    Add: methods to cors options

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 app.use(express.json());
 
